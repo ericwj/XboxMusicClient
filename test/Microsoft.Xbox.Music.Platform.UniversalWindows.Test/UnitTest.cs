61f2d1eee1704efe1922915a3380b12e5ea30855
Eric <ericnl@live.com>
2016-09-17 16:53:49 +0200

    Add UWP 5.2.2 test project

---
+++ test/Microsoft.Xbox.Music.Platform.UniversalWindows.Test/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Microsoft.Xbox.Music.Platform.UniversalWindows.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
